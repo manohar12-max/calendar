9422ebb52231aaff05ae0175e6f693f9e32b931d
Manohar Dhumal <2898manohar@gmail.com>
2025-06-14 13:12:42 +0530

    "fith commit"

--- calender/src/hooks/useGetArrayOfDays.jsx
+++ calender/src/hooks/useGetArrayOfDays.jsx
@@ -7,12 +7,12 @@ export function useGetArrayOfDays(year, month) {
 
   const daysArray = [];
 
-  // Add null placeholders before the first day of the month
+
   for (let i = 0; i < firstDay; i++) {
     daysArray.push(null);
   }
 
-  // Add actual days
+ 
   for (let day = 1; day <= daysInMonth; day++) {
     
     const fullDate = new Date(year, month, day);
